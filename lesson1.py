88a39a8d53fb290fe698e46bd87c2d9591bdf08f
fenenche <enenchef546@gmail.com>
2026-09-23 10:40:17 +0100

    lesson1.py

---
+++ lesson1.py
@@ -0,0 +1,19 @@
+print("My name is Favour")
+print("I am 20 years old")
+print("My favourite propraming language is python")
+
+
+name = "Favour"
+age = 20
+favourite_language = "Python"
+
+print(name)
+print(age)
+print(favourite_language)
+
+
+name = "Favour"
+age = 20
+
+print("My name is", name)
+print("I am", age, "years old")
