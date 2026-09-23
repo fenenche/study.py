da482f23347f0fa93ad94cd5dc27d05d6fdf6ac4
fenenche <enenchef546@gmail.com>
2026-09-23 11:42:27 +0100

    lesson3.py

---
+++ lesson3.py
@@ -0,0 +1,7 @@
+first_number = int(input("Enter a number: "))
+second_number = int(input("Enter another number: "))
+
+print("Addition:", first_number + second_number)
+print("Subtraction:", first_number - second_number)
+print("Multiplication:", first_number * second_number)
+print("Division:", first_number / second_number)
