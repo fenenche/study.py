18e53d168d2e1893e274be1d3375cc3ced0deaad
fenenche <enenchef546@gmail.com>
2026-09-23 11:16:51 +0100

    Add Python lesson 2

---
+++ lesson2.py
@@ -0,0 +1,7 @@
+name = input("What is your name? ")
+age = input("How old are you? ")
+language = input("What is your favourite programming language? ")
+
+print("My name is", name)
+print("I am", age, "years old")
+print("My favourite programing language is", language)
