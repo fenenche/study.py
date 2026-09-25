16b15f4754856f87081297934ce0ef160dafdeaf
fenenche <enenchef546@gmail.com>
2026-09-25 16:05:36 +0100

    lesson7.py

---
+++ lesson7.py
@@ -0,0 +1,16 @@
+for i in range(2):
+    print("My name favour")
+
+for i in range(4):
+    print("I love python")
+
+for i in range(3):
+    print("I am learning python")
+
+for learning_hours in range(5):
+    print("My learning_hours")
+
+
+learning_hours = 5
+for learning_hours in range(1, 5):
+    print("I studied for", learning_hours, "hours")
